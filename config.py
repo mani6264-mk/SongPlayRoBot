4d9dcc3cf07dc88c0208bfe441115d4a69943a10
mani6264-mk <mktamilan6264@gmail.com>
2025-04-06 09:32:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "28841231"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "282681a770c339f303e8103d88a5998e" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7951737464:AAHYwd1KTjLh3ZvgiLZgLbXtdgHLzEHfT6M" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
